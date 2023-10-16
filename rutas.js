2a52b82f6f8dbfde39aaa1a3e0e6c4c30b2877ba
David Parra <davidparra0101@gmail.com>
2023-10-16 16:58:23 -0500

    Actualizar

--- rutas.js
+++ rutas.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const axios = require('axios');
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
+routes.get('/lista', (req, res) => {
     req.getConnection((err, conn) => {
         if(err) return res.send(err)
 
@@ -13,7 +14,7 @@ routes.get('/', (req, res) => {
     })
 })
 
-routes.post('/', (req, res) => {
+routes.post('/lista/insertar', (req, res) => {
     req.getConnection((err, conn) => {
         const { idcliente, idproducto } = req.body;
 
@@ -29,7 +30,7 @@ routes.post('/', (req, res) => {
     })
 })
 
-routes.delete('/:idproducto', (req, res) => {
+routes.delete('/lista/eliminar/:idproducto', (req, res) => {
     req.getConnection((err, conn) => {
         if(err) return res.send(err)
 
@@ -41,4 +42,69 @@ routes.delete('/:idproducto', (req, res) => {
     })
 })
 
+routes.get('/lista/:idcliente', (req, res) => {
+    const idcliente = req.params.idcliente;
+
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+
+        conn.query('SELECT idproducto FROM productos WHERE idcliente = ?', [idcliente], (err, rows) => {
+            if (err) return res.send(err);
+
+            const idProductos = rows.map(row => row.idproducto);
+
+            res.json(idProductos);
+        });
+    });
+});
+  
+routes.get('/actualizar', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('SELECT idproducto FROM productos', (err, rows) => {
+            if (err) return res.send(err);
+
+            const idproducto_vector = rows.map((row) => row.idproducto);
+
+            const apiUrls = [
+                'https://cards.thenexusbattles2.cloud/api/cartas/?size=1000&page=1&coleccion=Armas&onlyActives=true',
+                'https://cards.thenexusbattles2.cloud/api/cartas/?size=1000&page=1&coleccion=Heroes&onlyActives=true',
+                'https://cards.thenexusbattles2.cloud/api/cartas/?size=1000&page=1&coleccion=Armaduras&onlyActives=true',
+                'https://cards.thenexusbattles2.cloud/api/cartas/?size=1000&page=1&coleccion=Epicas&onlyActives=true',
+                'https://cards.thenexusbattles2.cloud/api/cartas/?size=1000&page=1&coleccion=Items&onlyActives=true',
+            ];
+
+            const apiPromises = apiUrls.map(apiUrl => axios.get(apiUrl).then(response => response.data));
+
+            Promise.all(apiPromises)
+                .then(apiResponses => {
+                    const _id_vector = apiResponses
+                        .map(apiResponse => apiResponse.map(item => item._id))
+                        .flat();
+
+                    const valoresAEliminar = idproducto_vector.filter(id => !_id_vector.includes(id));
+
+                    if (valoresAEliminar.length > 0) {
+                        conn.query('DELETE FROM productos WHERE idproducto IN (?)', [valoresAEliminar], (err, result) => {
+                            if (err) return res.send(err);
+
+                            console.log(`Se eliminaron ${result.affectedRows} registros.`);
+                        });
+                    } else {
+                        console.log('No se encontraron registros para eliminar.');
+                    }
+
+                    res.json({
+                        idproducto_vector,
+                        _id_vector,
+                    });
+                })
+                .catch((error) => {
+                    console.error('Error al consumir las APIs:', error);
+                    res.status(500).json({ error: 'Error al consumir las APIs' });
+                });
+        });
+    });
+});
+  
 module.exports = routes
